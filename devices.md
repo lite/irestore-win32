ad1ab0f2568acb9b2778f90fafb4e2c08f2eb0f3
lite <litekok@gmail.com>
2011-08-23 00:38:00 +0800

    iphone1 ATT 3.1.3 ipsw

---
+++ devices.md
@@ -0,0 +1,54 @@
+ATT iphone 3.1.3
+====
+
+{"BasebandThumbprint"=>"571673BFEEE8E6AE1F6CFEFD1D6DBC9526B2A9BC",
+ "FirmwareVersion"=>"iBoot-636.66.33",
+ "MLBSerialNumber"=>"",
+ "ProductVersion"=>"3.1.3",
+ "TimeIntervalSince1970"=>1314024405,
+ "WiFiAddress"=>"00:1c:b3:6e:ef:7f",
+ "DeviceName"=>"iPhone",
+ "ActivationPublicKey"=>
+  "-----BEGIN RSA PUBLIC KEY----- 
+MIGIAoGAb7hgZjno/6RK4zxH8C8rYWM90HuBDQAfkpB29b6KyuVd7RA1vSrFD6IH 
+7uYWZrg5z6/tn2mfkpsNziJN3a9p72LajozdBGDv5EMze4uPkQxUxr55Sl0CTbqF 
+PdJCqY8vo25eBJZyZn1ncjOZY8+XPX+nzGc/Dvt0QHFV6loi8gECAwEAAQ== 
+-----END RSA PUBLIC KEY----- 
+",
+ "ProductType"=>"iPhone1,1",
+ "ProtocolVersion"=>"2",
+ "UniqueChipID"=>1394925108686,
+ "BuildVersion"=>"7E18",
+ "HardwareModel"=>"M68AP",
+ "SBLockdownEverRegisteredKey"=>false,
+ "TimeZone"=>"Asia/Harbin",
+ "TrustedHostAttached"=>true,
+ "BasebandBootloaderVersion"=>"3.9_M3S2",
+ "DeviceClass"=>"iPhone",
+ "DevicePublicKey"=>
+  "-----BEGIN RSA PUBLIC KEY----- 
+MIGIAoGAcPM982CxZraM4l1WVGY7EjaHRfAf379tnxdCuZsLuThmKPXIJcUz5UTX 
+e3YXV4rFy2TmONP6hqODrRpNYaqPySxiAVqlhfodRc2BQLntY1pYVFdRaqV/mRvI 
+4T6hIgPp2SLFnKQX3J/KQAioEnRWAdbkiwC/wog03LaXUazC7scCAwEAAQ== 
+-----END RSA PUBLIC KEY----- 
+",
+ "BasebandMasterKeyHash"=>"E93B43F3EF6DAED516A2D4B9BAD5494DC81E92D3",
+ "HostAttached"=>true,
+ "SoftwareBehavior"=>
+  "\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000",
+ "TimeZoneOffsetFromUTC"=>28800.0,
+ "InternationalMobileEquipmentIdentity"=>"011245007201252",
+ "RegionInfo"=>"LL/A",
+ "UniqueDeviceID"=>"4f098924a38e736de78eb524c2b7cfd7ccf23454",
+ "ActivationState"=>"Unactivated",
+ "BasebandVersion"=>"04.05.04_G",
+ "CPUArchitecture"=>"armv6",
+ "ProductionSOC"=>true,
+ "SerialNumber"=>"7R7287VYWH8",
+ "BluetoothAddress"=>"00:1c:b3:6e:ef:7e",
+ "Uses24HourClock"=>false,
+ "ModelNumber"=>"MA712",
+ "SIMStatus"=>"kCTSIMSupportSIMStatusNotInserted",
+ "SomebodySetTimeZone"=>true,
+ "PasswordProtected"=>false}
+
--- ipsw_ext.rb
+++ ipsw_ext.rb
@@ -15,7 +15,6 @@ PATH_BASE           = File.expand_path("~/tools/iOS")
 p PATH_BASE
 
 PATH_DMG            = File.join(PATH_BASE, "ipsw/dmg")
-FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifest.plist")
 FILE_MANIFEST       = File.join(PATH_DMG, "Firmware/all_flash/all_flash.n88ap.production/manifest")
 # 
 PATH_DMG_NEW        = File.join(PATH_BASE, "ipsw/dmg_new")
@@ -27,17 +26,28 @@ FILE_KERNELCACHE    = File.join(PATH_DMG_NEW, "kernelcache.release.n88")
 FILE_LLB            = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.n88ap.production/LLB.n88ap.RELEASE.img3")
 FILE_IMGDIR         = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.n88ap.production")
 
+#### iphone 3gs
 ### 4.3.3
 # FILE_IPSW           = File.join(PATH_BASE, "iPhone2,1_4.3.3_8J2_Restore.ipsw")
 # FILE_RESTOREDMG     = File.join(PATH_DMG, "038-1417-003.dmg")
 # FILE_RAMDISK        = File.join(PATH_DMG_NEW, "038-1447-003.dmg")
+# FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifest.plist")
 
 ### 4.3.4
 # FILE_IPSW           = File.join(PATH_BASE, "iPhone2,1_4.3.4_8K2_Restore.ipsw")
 # FILE_RESTOREDMG     = File.join(PATH_DMG, "038-2191-001.dmg")
 # FILE_RAMDISK        = File.join(PATH_DMG_NEW, "038-2169-001.dmg")
+# FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifest.plist")
 
 ### 4.3.5
-FILE_IPSW           = File.join(PATH_BASE, "iPhone2,1_4.3.5_8L1_Restore.ipsw")
-FILE_RESTOREDMG     = File.join(PATH_DMG, "038-2287-002.dmg")
-FILE_RAMDISK        = File.join(PATH_DMG_NEW, "038-2257-002.dmg")
+# FILE_IPSW           = File.join(PATH_BASE, "iPhone2,1_4.3.5_8L1_Restore.ipsw")
+# FILE_RESTOREDMG     = File.join(PATH_DMG, "038-2287-002.dmg")
+# FILE_RAMDISK        = File.join(PATH_DMG_NEW, "038-2257-002.dmg")
+# FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifest.plist")
+
+#### iphone
+### 3.1.3
+FILE_IPSW           = File.join(PATH_BASE, "iPhone1,1_3.1.3_7E18_Restore.ipsw")
+FILE_RESTOREDMG     = File.join(PATH_DMG, "018-6482-014.dmg")
+FILE_RAMDISK        = File.join(PATH_DMG_NEW, "018-6494-014.dmg")
+FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifesto.plist")
