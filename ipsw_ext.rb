762e44d6a6e64552f319bfea0fbe3e7ed20c488e
lite <litekok@gmail.com>
2011-10-10 23:57:55 +0800

    define function to get ipsw info

--- ipsw_ext.rb
+++ ipsw_ext.rb
@@ -2,7 +2,7 @@
 # encoding: utf-8
 
 $: << File.dirname(__FILE__)
-                  
+
 require 'rubygems'
 
 if /darwin/ =~ RUBY_PLATFORM
@@ -11,55 +11,108 @@ else
   PATH_IRECOVERY = File.join(File.dirname(__FILE__), "s-irecovery.exe");
 end
 
-PATH_BASE           = File.expand_path("~/tools/iOS")
+PATH_BASE = File.expand_path("~/tools/iOS")
 p PATH_BASE
 
-PATH_DMG            = File.join(PATH_BASE, "ipsw/dmg")
+PATH_DMG = File.join(PATH_BASE, "ipsw/dmg")
 
-# 
+#
 #### iphone 3gs
-DEVICE_BOARDCONFIG  = "n88ap"
-PATH_DMG_NEW        = File.join(PATH_BASE, "ipsw/dmg_new")
-FILE_KERNELCACHE    = File.join(PATH_DMG_NEW, "kernelcache.release.n88")
-FILE_APPLELOG       = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/applelogo.s5l8920x.img3")
-
-### 4.3.3
-# FILE_IPSW           = File.join(PATH_BASE, "iPhone2,1_4.3.3_8J2_Restore.ipsw")
-# FILE_RESTOREDMG     = File.join(PATH_DMG, "038-1417-003.dmg")
-# FILE_RAMDISK        = File.join(PATH_DMG_NEW, "038-1447-003.dmg")
-# FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifest.plist")
-
-### 4.3.4
-# FILE_IPSW           = File.join(PATH_BASE, "iPhone2,1_4.3.4_8K2_Restore.ipsw")
-# FILE_RESTOREDMG     = File.join(PATH_DMG, "038-2191-001.dmg")
-# FILE_RAMDISK        = File.join(PATH_DMG_NEW, "038-2169-001.dmg")
-# FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifest.plist")
+DEVICE_BOARDCONFIG = "n88ap"
+PATH_DMG_NEW = File.join(PATH_BASE, "ipsw/dmg_new")
+FILE_KERNELCACHE = File.join(PATH_DMG_NEW, "kernelcache.release.n88")
+FILE_APPLELOG = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/applelogo.s5l8920x.img3")
 
 ### 4.3.5
-FILE_IPSW           = File.join(PATH_BASE, "iPhone2,1_4.3.5_8L1_Restore.ipsw")
-FILE_RESTOREDMG     = File.join(PATH_DMG, "038-2287-002.dmg")
-FILE_RAMDISK        = File.join(PATH_DMG_NEW, "038-2257-002.dmg")
+FILE_IPSW = File.join(PATH_BASE, "iPhone2,1_4.3.5_8L1_Restore.ipsw")
+FILE_RESTOREDMG = File.join(PATH_DMG, "038-2287-002.dmg")
+FILE_RAMDISK = File.join(PATH_DMG_NEW, "038-2257-002.dmg")
 FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifest.plist")
 
-#### iphone
-# DEVICE_BOARDCONFIG  = "m68ap"
-# PATH_DMG_NEW        = PATH_DMG
-# FILE_KERNELCACHE    = File.join(PATH_DMG_NEW, "kernelcache.release.s5l8900x")
-# FILE_APPLELOG       = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/applelogo.s5l8900x.img3")
-
-### 3.1.3
-# FILE_IPSW           = File.join(PATH_BASE, "iPhone1,1_3.1.3_7E18_Restore.ipsw")
-# FILE_RESTOREDMG     = File.join(PATH_DMG, "018-6482-014.dmg")
-# FILE_RAMDISK        = File.join(PATH_DMG_NEW, "018-6494-014.dmg")
-# FILE_MANIFEST_PLIST = File.join(PATH_DMG, "BuildManifesto.plist")
-
 # img3 files
-FILE_MANIFEST       = File.join(PATH_DMG, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/manifest")
-FILE_IBEC           = File.join(PATH_DMG_NEW, "Firmware/dfu/iBEC.#{DEVICE_BOARDCONFIG}.RELEASE.dfu")
-FILE_DEVICETREE     = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/DeviceTree.#{DEVICE_BOARDCONFIG}.img3")
-FILE_LLB            = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/LLB.#{DEVICE_BOARDCONFIG}.RELEASE.img3")
-FILE_IMGDIR         = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production")
+FILE_MANIFEST = File.join(PATH_DMG, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/manifest")
+FILE_IBEC = File.join(PATH_DMG_NEW, "Firmware/dfu/iBEC.#{DEVICE_BOARDCONFIG}.RELEASE.dfu")
+FILE_DEVICETREE = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/DeviceTree.#{DEVICE_BOARDCONFIG}.img3")
+FILE_LLB = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production/LLB.#{DEVICE_BOARDCONFIG}.RELEASE.img3")
+FILE_IMGDIR = File.join(PATH_DMG_NEW, "Firmware/all_flash/all_flash.#{DEVICE_BOARDCONFIG}.production")
 
-### unzip
-#system("mkdir -p #{PATH_DMG}")
-#system("unzip -d #{PATH_DMG} #{FILE_IPSW}")
+def unzip_ipsw
+  system("mkdir -p #{PATH_DMG}")
+  system("unzip -d #{PATH_DMG} #{FILE_IPSW}")
+end
+
+def firmware_info(model, ipsw)
+  get_firmware_files(model, ipsw)
+end
+
+def get_firmware_info_table
+  {
+      # iphone 2g
+      "m68ap"=> {
+          :file_manifest_plist => "BuildManifest.plist",
+          :file_manifest => "Firmware/all_flash/all_flash.m68ap.production/manifest",
+
+          :file_imgdir => "Firmware/all_flash/all_flash.m68ap.production",
+          :file_kernelcache => "kernelcache.release.s5l8900x",
+          :file_applelog => "Firmware/all_flash/all_flash.m68ap.production/applelogo.s5l8900x.img3",
+          :file_ibec => "Firmware/dfu/iBEC.m68ap.RELEASE.dfu",
+          :file_devicetree => "Firmware/all_flash/all_flash.m68ap.production/DeviceTree.m68ap.img3",
+          :file_llb => "Firmware/all_flash/all_flash.m68ap.production/LLB.m68ap.RELEASE.img3",
+
+          "ios3_1_3" => {
+              :file_ipsw => "iPhone1,1_3.1.3_7E18_Restore.ipsw",
+              :file_restoredmg => "018-6482-014.dmg",
+              :file_ramdisk => "018-6494-014.dmg",
+          },
+      },
+      # iphone 3gs
+      "n88ap"=> {
+          :file_manifest_plist => "BuildManifest.plist",
+          :file_manifest => "Firmware/all_flash/all_flash.n88ap.production/manifest",
+
+          :file_imgdir => "Firmware/all_flash/all_flash.n88ap.production",
+          :file_kernelcache => "kernelcache.release.n88",
+          :file_applelog => "Firmware/all_flash/all_flash.n88ap.production/applelogo.s5l8920x.img3",
+          :file_ibec => "Firmware/dfu/iBEC.n88ap.RELEASE.dfu",
+          :file_devicetree => "Firmware/all_flash/all_flash.n88ap.production/DeviceTree.n88ap.img3",
+          :file_llb => "Firmware/all_flash/all_flash.n88ap.production/LLB.n88ap.RELEASE.img3",
+
+          "ios4_3_5" => {
+              :file_ipsw => "iPhone2,1_4.3.5_8L1_Restore.ipsw",
+              :file_restoredmg => "038-2287-002.dmg",
+              :file_ramdisk => "038-2257-002.dmg",
+          },
+
+          "ios5_0" => {
+              :file_ipsw => "iPhone2,1_5.0_9A334_Restore.ipsw",
+              :file_restoredmg => "dmg/018-7873-736.dmg",
+              :file_ramdisk => "018-7919-343.dmg",
+          },
+      }
+  }
+end
+
+def get_info(model, ipsw_ver)
+  info = get_firmware_info_table
+
+  path_base = File.expand_path("~/tools/iOS")
+  path_dmg = File.join(path_base, "ipsw/dmg")
+  path_dmg_new = File.join(path_base, "ipsw/dmg_new")
+
+  {
+      :file_manifest_plist => File.join(path_dmg, info[model][:file_manifest_plist]),
+      :file_manifest => File.join(path_dmg, info[model][:file_manifest]),
+
+      :file_imgdir => File.join(path_dmg_new, info[model][:file_imgdir]),
+      :file_kernelcache =>File.join(path_dmg_new, info[model][:file_kernelcache]),
+      :file_applelog =>File.join(path_dmg_new, info[model][:file_applelog]),
+      :file_ibec => File.join(path_dmg_new, info[model][:file_ibec]),
+      :file_devicetree => File.join(path_dmg_new, info[model][:file_devicetree]),
+      :file_llb => File.join(path_dmg_new, info[model][:file_llb]),
+
+      :file_ipsw => File.join(path_base, info[model][ipsw_ver][:file_ipsw]),
+      :file_restoredmg => File.join(path_dmg, info[model][ipsw_ver][:file_restoredmg]),
+      :file_ramdisk => File.join(path_dmg_new, info[model][ipsw_ver][:file_ramdisk]),
+  }
+end
+
---
+++ test_ipsw_ext.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+$: << File.dirname(__FILE__)
+
+require 'rubygems'
+require 'ipsw_ext'
+require 'pp'
+
+def test_get_iphone3gs_ios5
+  pp get_info("n88ap", "ios5_0")
+end
+
+def test_get_iphone3gs_ios4
+  pp get_info("n88ap", "ios4_3_5")
+end
+
+def test_get_iphone2g_ios3
+  pp get_info("m68ap", "ios3_1_3")
+end
+
+if __FILE__ == $0
+  test_get_iphone3gs_ios5
+  #test_get_iphone3gs_ios4
+  #test_get_iphone2g_ios3
+end
