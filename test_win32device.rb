b5b06608127e1960aa3944311fb3ec2960b19b12
lite <litekok@gmail.com>
2011-10-10 17:54:21 +0800

    enter restore mode in test script

--- test_win32device.rb
+++ test_win32device.rb
@@ -6,93 +6,104 @@ $: << File.dirname(__FILE__)
 require 'rubygems'
 require 'win32device'
 
-def test_getdevpath
-  dev = Win32Device.new
+def test_getdevpath dev
   dev.get_iboot
 end
 
-def test_send_command(cmd, request=0)
-  dev = Win32Device.new
-  path = dev.get_iboot
-  puts path
-  dev.open path
-  dev.send_command(cmd, request)
+def test_reboot dev
+  dev.open
+  dev.send_command "reboot"
   dev.close
 end
 
-def test_recv_command
-  dev = Win32Device.new
-  path = dev.get_iboot
-  puts path
-  dev.open path
+def test_getenv dev
+  dev.open
+  cmd = "getenv build-version"
+  dev.send_command(cmd)
   puts dev.recv_command
   dev.close
 end
 
-def test_reboot
-  test_send_command "reboot"
-end
-
-def test_getenv
-  test_send_command "getenv build-version"
-end
-
-def test_send_file(filename)
-  dev = Win32Device.new
-  path = dev.get_iboot
-  puts path
-  dev.open path
-  dev.send_file(filename)
+def test_set_interface dev
+  dev.open
+  dev.set_interface(1, 0)
   dev.close
 end
 
-def test_read_file
-  filename = '/home/dli/tools/iOS/ipsw/dmg_new/Firmware/dfu/iBEC.n88ap.RELEASE.dfu'
-  File.open(filename) do |f|
-    while buffer = f.read(0x800) do
-      puts buffer.size
-    end
-  end
-end
+def test_ibec dev
+  dev.open
+  #dev.reset
+  #dev.init
+  #dev.set_config(1)
+  #dev.set_interface(1, 0)
 
-def test_control_transfer(request_type, request, value, index, in_buffer, timeout=1000)
-  dev = Win32Device.new
-  path = dev.get_iboot
-  puts path
-  dev.open path
-  dev.control_transfer(request_type, request, value, index, in_buffer, timeout)
-  dev.close
-end
-
-def test_set_interface
-  dev = Win32Device.new
-  path = dev.get_iboot
-  puts path
-  dev.open path
-  dev.control_transfer(0x01, 0x0b, 0, 1, nil)
-  dev.close
-end
-
-def test_ibec
-  test_set_interface
-
-  test_send_command("setenv auto-boot false")
-  test_send_command("saveenv")
-
-  test_control_transfer(0x41, 0, 0, 0, nil)
+  dev.send_command("setenv auto-boot false")
+  dev.send_command("saveenv")
 
   p "sending iBEC"
   filename = '/home/dli/tools/iOS/ipsw/dmg_new/Firmware/dfu/iBEC.n88ap.RELEASE.dfu'
-  test_send_file(filename)
+  dev.send_file(filename)
 
-  test_control_transfer(0x40, 0x1, 0, 0, "go\0")
+  dev.send_command("go", 0x1)
+
+  dev.close
+end
+
+def test_enter_mode  dev
+  dev.open
+  dev.init
+  dev.set_interface(1, 0)
+
+  test_applogo dev
+  test_ramdisk dev
+  test_devicetree dev
+  test_kernel dev
+
+  dev.reset
+  dev.close
+end
+
+def test_applogo dev
+  p "sending apple logo"
+  filename = '/home/dli/tools/iOS/ipsw/dmg_new/Firmware/all_flash/all_flash.n88ap.production/applelogo.s5l8920x.img3'
+  dev.send_file(filename)
+
+  dev.send_command("setpicture 0")
+  dev.send_command("bgcolor 0 0 0")
+end
+
+def test_ramdisk dev
+  p "sending ramdisk"
+  filename = '/home/dli/tools/iOS/ipsw/dmg_new/038-2257-002.dmg'
+  dev.send_file(filename)
+
+  dev.send_command("ramdisk")
+end
+
+def test_devicetree dev
+  p "sending device tree"
+  filename = '/home/dli/tools/iOS/ipsw/dmg_new/Firmware/all_flash/all_flash.n88ap.production/DeviceTree.n88ap.img3'
+  dev.send_file(filename)
+
+  dev.send_command("devicetree")
+end
+
+def test_kernel dev
+  p "sending kernel"
+  filename = '/home/dli/tools/iOS/ipsw/dmg_new/kernelcache.release.n88'
+  dev.send_file(filename)
+
+  p "booting"
+  dev.send_command("setenv boot-args rd=md0 nand-enable-reformat=1 -progress")
+  dev.send_command("bootx", 0x1)
 end
 
 if __FILE__ == $0
-  #test_getdevpath
-  #test_getenv
-  #test_recv_command
-  #test_reboot
-  #test_read_file
-  test_ibec
+  dev = Win32Device.new
+  #test_reboot dev
+  #test_getdevpath dev
+  #test_set_interface dev
+  #test_getenv dev
+  test_ibec dev
+  #test_enter_mode dev
 end
--- win32device.rb
+++ win32device.rb
@@ -5,13 +5,14 @@ $: << File.dirname(__FILE__)
 
 require 'rubygems'
 require "Win32API"
-require "pp"
 
 #static const GUID GUID_DEVINTERFACE_IBOOT = {0xED82A167L, 0xD61A, 0x4AF6, {0x9A, 0xB6, 0x11, 0xE5, 0x22, 0x36, 0xC5, 0x76}};
 #static const GUID GUID_DEVINTERFACE_DFU = {0xB8085869L, 0xFEB9, 0x404B, {0x8C, 0xB1, 0x1E, 0x5C, 0x14, 0xFA, 0x8C, 0x54}};
 
 #TRUE = 1
 #FALSE = 0
+
+#INFINITE = 0xFFFFFFFF
 
 #GENERIC_READ    = 0x80000000
 #GENERIC_WRITE   = 0x40000000
@@ -32,12 +33,12 @@ require "pp"
 class Win32Device
 
   def initialize
-    @createFile = Win32API.new('kernel32', 'CreateFile', 'PLLPLLL', 'L')
+    @createFile = Win32API.new('kernel32', 'CreateFile', 'PLLPLLP', 'L')
     @deviceIoControl = Win32API.new('kernel32', 'DeviceIoControl', 'LLPLPLPP', 'I')
     @closeHandle = Win32API.new("kernel32", "CloseHandle", ["L"], 'L')
 
-    @createEvent = Win32API.new('kernel32', 'CreateEvent', 'PIIP', 'L')
-    @getOverlappedResult = Win32API.new('kernel32', 'GetOverlappedResult', 'LPPI', 'I')
+    @createEvent = Win32API.new('kernel32', 'CreateEvent', 'PLLP', 'L')
+    @getOverlappedResult = Win32API.new('kernel32', 'GetOverlappedResult', 'LPPL', 'L')
     @waitForSingleObject = Win32API.new('kernel32', 'WaitForSingleObject', 'LL', 'L')
     @cancelIo = Win32API.new("kernel32", "CancelIo", 'L', 'L')
 
@@ -49,9 +50,19 @@ class Win32Device
     @setupDiDestroyDeviceInfoList = Win32API.new('setupapi', 'SetupDiDestroyDeviceInfoList', 'L', 'B')
   end
 
+  def close
+    @closeHandle.call(@h)
+    @h = nil
+  end
+
+  def open
+    path = get_iboot
+    @h = @createFile.call(path, 0xc0000000, 0x3, nil, 0x3, 0x40000000, 0)
+    #@h = @createFile.call(path, 0xc0000000, 0, nil, 0x3, 0, nil)
+  end
+
   def get_dfu
-    puts "get_dfu"
-    uuid = [0xB8085869, 0xFEB9, 0x404B, 0x8C, 0xB1, 0x1E, 0x5C, 0x14, 0xFA, 0x8C, 0x54].pack('LSSCCCCCCCC')    # DFU
+    uuid = [0xB8085869, 0xFEB9, 0x404B, 0x8C, 0xB1, 0x1E, 0x5C, 0x14, 0xFA, 0x8C, 0x54].pack('LSSCCCCCCCC') # DFU
     get_devpath(uuid)
   end
 
@@ -69,58 +80,35 @@ class Win32Device
       spdid = [0x18+0x4].pack('L')+[0x00].pack('C')*0x18
       @setupDiEnumDeviceInterfaces.call(hdi, nil, uuid, idx, spdid)
       error = @getLastError.call()
-      pp "setupDiEnumDeviceInterfaces #{error}, #{idx}"
       break if error == 259
       idx += 1
       required_size = [0x00].pack('L')
       @setupDiGetDeviceInterfaceDetail.call(hdi, spdid, nil, 0, required_size, nil)
       buffer_size = required_size.unpack('L')[0]
-      pp "setupDiGetDeviceInterfaceDetail", required_size, buffer_size
       spdidd = [0x5].pack('L') + [0x00].pack('C')*(buffer_size-0x4) # here size must be 0x5
       @setupDiGetDeviceInterfaceDetail.call(hdi, spdid, spdidd, buffer_size, nil, nil)
-      pp "setupDiGetDeviceInterfaceDetail #{dev_path}, #{buffer_size}", spdidd
       dev_path = spdidd[4..-1].to_s
     end
     @setupDiDestroyDeviceInfoList.call(hdi)
     dev_path
   end
 
-  def open path
-    @h = @createFile.call(path, 0xc0000000, 0x3, 0, 0x3, 0x40000000, 0)
-    puts @h
-  end
 
   def send_command(cmd, request=0)
-    control_transfer(0x40, request, 0, 0, cmd + "\0")
+    control_command(0x40, request, 0, 0, cmd + "\0")
   end
 
   def recv_command
-    control_transfer(0xc0, 0, 0, 0, nil)
-  end
-
-  def print_progress_bar(progress)
-      if progress < 0
-        return
-      elsif progress > 100
-        progress = 100
-      end
-      printf "\r[";
-      (0..50).each do |i|
-        if(i < progress / 2)
-          printf "=";
-        else
-          printf " " ;
-        end
-      end
-    printf "] #{progress}%%"
-      printf "\n"  if progress == 100;
+    control_transfer(0xc0, 0, 0, 0, 0x400)
   end
 
   def send_file(filename)
+    control_transfer(0x41, 0, 0, 0, 0)
+
     total_size = File.stat(filename).size
     packet_size = 0
     File.open(filename, 'r') do |fp|
-      while buffer = fp.read(0x800) do
+      while buffer = fp.read(0x2000) do
         bulk_transfer(buffer)
         packet_size += buffer.size
         print_progress_bar(packet_size*100/total_size)
@@ -129,53 +117,90 @@ class Win32Device
     print_progress_bar(100)
   end
 
-  def control_transfer(request_type, request, value, index, in_buffer, timeout=1000)
-    packet = [request_type, request, value, index].pack('CCSS')
-    out_buffer = "\x00" * 0x400
+  def reset
+    bytes_transferred = [0x00].pack('L')
+    @deviceIoControl.call(@h, 0x22000C, nil, 0, nil, 0, bytes_transferred, nil);
+  end
 
-    case request_type
-    when 0x40
-      packet += [in_buffer.size].pack('S')
-      packet += in_buffer
-    when 0xc0
-      packet += [out_buffer.size].pack('S')
-      packet += out_buffer
-    else
-      packet += [0x00].pack('S')
-    end
+  def init
+    #40 00 00 00  00 00 13 00
+    control_transfer(0x40, 0, 0, 0, 0x13)
+  end
 
-    puts "deviceIoControl"
+  def set_config(config)
+    #00 09 01 00  00 00 00 00
+    control_transfer(0x00, 0x09, config, 0, 0)
+  end
+
+  def set_interface(interface, alt_setting)
+    #01 0b 00 00  01 00 00 00
+    control_transfer(0x01, 0x0b, alt_setting, interface, 0)
+  end
+
+  def control_command(request_type, request, value, index, in_buffer=nil)
+    packet = [request_type, request, value, index, in_buffer.size].pack('CCSSS')
+    packet += in_buffer
+
+    control_io(in_buffer.size, packet)
+  end
+
+  def control_transfer(request_type, request, value, index, length)
+    packet = [request_type, request, value, index, length].pack('CCSSS')
+    packet += "\x00" * length if length > 0
+
+    control_io(length, packet)
+  end
+
+  def control_io(length, packet)
+    control_io_overlapped(length, packet)
+    return
+    out_buffer = "\0"*length
+    bytes_transferred = [0x00].pack('L')
+    @deviceIoControl.call(@h, 0x2200A0, packet, packet.size, out_buffer, out_buffer.size, bytes_transferred, nil)
+    transferred = bytes_transferred.unpack('L')[0]
+    out_buffer[0, transferred]
+  end
+
+  def control_io_overlapped(length, packet, timeout=1000)
+    out_buffer = "\0"*length
     event = @createEvent.Call(nil, 1, 0, nil)
     overlapped = [0, 0, 0, 0, event].pack("L*")
     bytes_transferred = [0x00].pack('L')
-    ctrl_code = 0x2200A0
-    @deviceIoControl.call(@h, ctrl_code, packet, packet.size, out_buffer, out_buffer.size, bytes_transferred, overlapped)
-    puts "waitForSingleObject"
+    @deviceIoControl.call(@h, 0x2200A0, packet, packet.size, out_buffer, out_buffer.size, bytes_transferred, overlapped)
     @waitForSingleObject.call(event, timeout)
-    puts "getOverlappedResult"
-    @getOverlappedResult.Call(@h, overlapped, bytes_transferred, 0)
-    puts "closeHandle"
-    @closeHandle.call(@h)
+    ret = @getOverlappedResult.Call(@h, overlapped, bytes_transferred, 0)
+    @closeHandle.call(event)
+    @cancelIo.call(@h) unless ret
+    puts "cancelIo" unless ret
+
     transferred = bytes_transferred.unpack('L')[0]
-    pp "#{transferred}", out_buffer[0, transferred]
     out_buffer[0, transferred]
   end
 
   def bulk_transfer(packet)
     bytes_transferred = [0x00].pack('L')
-    ctrl_code = 0x2201B6 #0x220003 #0x220195 #0x2201B6
-    @deviceIoControl.call(@h, ctrl_code, packet, packet.size, packet, packet.size, bytes_transferred, nil)
-    transferred = bytes_transferred.unpack('L')[0]
-    pp "#{transferred}"
+    #0x220003 #0x220195 #0x2201B6
+    @deviceIoControl.call(@h, 0x2201B6, packet, packet.size, packet, packet.size, bytes_transferred, nil)
+    bytes_transferred.unpack('L')[0]
   end
 
-  def reset
-    count = 0;
-    @deviceIoControl.call(@h, 0x22000C, 0, 0, 0, 0, count, 0);
+  private
+  def print_progress_bar(progress)
+    if progress < 0
+      return
+    elsif progress > 100
+      progress = 100
+    end
+    printf "\r[";
+    (0..50).each do |i|
+      if (i < progress / 2)
+        printf "=";
+      else
+        printf " ";
+      end
+    end
+    printf "] #{progress}%%"
+    printf "\n" if progress == 100;
   end
 
-  def close
-    @closeHandle.call(@h)
-    @h = nil
-  end
 end
